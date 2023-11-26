e486c169a67d47877fa918e09c6e904b2f2c9458
Lawal Taofik <layomide81@gmail.com>
2023-11-26 12:22:07 +0100

    abandon sidebar

--- src/components/Pages/Sidebar.jsx
+++ src/components/Pages/Sidebar.jsx
@@ -10,62 +10,67 @@ import client from "../../assets/logos/client-profile-logo.svg";
 import help from "../../assets/logos/help-logo.svg";
 import dashboard from "../../assets/logos/dashboard-logo.svg"; 
 import settings from "../../assets/logos/settings-logo.svg"; 
+// ... (your imports)
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
 
   const Menus = [
-    { title: "Dashboard",  logo: dashboard },
-    { title: "Client",  client},
-    { title: "Payment",  payment },
-    { title: "Invoice",  invoice },
-    { title: "Tracking",  tracking},
-    { title: "Notification",  notification },
-    { title: "To-Do List",  todo },
-    { title: "Help Center",  help , gap: true},
-    { title: "Setting", settings },
+    { title: "Dashboard", logo: dashboard },
+    { title: "Client", logo: client },
+    { title: "Payment", logo: payment },
+    { title: "Invoice", logo: invoice },
+    { title: "Tracking", logo: tracking },
+    { title: "Notification", logo: notification },
+    { title: "To-Do List", logo: todo },
+    { title: "Help Center", logo: help, gap: true },
+    { title: "Setting", logo: settings },
   ];
 
   return (
     <div className="flex">
       <div
-        className={` ${
+        className={`${
           open ? "w-72" : "w-20 "
         } bg-customBlue h-screen p-5  pt-8 relative duration-300`}
       >
         <img
           src={control}
           alt="control button"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
-           border-2 rounded-full  ${!open && "rotate-180"}`}
+          className={`absolute cursor-pointer -right-3 top-10 w-5   ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)}
         />
-        <div className="flex gap-x-4 items-center">
+        <div className="flex flex-col items-center gap-y-2">
           <img
             src={paypro}
-            alt="control-buttton"
+            alt="paypro logo"
             className={`cursor-pointer duration-500 ${
               open && "rotate-[360deg]"
             }`}
           />
           <h1
-            className={`text-white origin-left font-medium text-xl duration-200 ${
+            className={`text-white font-light text-xs duration-200 ${
               !open && "scale-0"
             }`}
           >
-            Designer
+            Service Management Board
           </h1>
         </div>
+        <div className="border-b border-gray-400 my-4"></div>
         <ul className="pt-6">
           {Menus.map((Menu, index) => (
             <li
               key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              className={`flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
               ${Menu.gap ? "mt-9" : "mt-2"} ${
                 index === 0 && "bg-light-white"
-              } `}
+              }`}
             >
-              {Menu.icon}
+              <img
+                src={Menu.logo}
+                alt={Menu.title}
+                className={`${!open && "hidden"} origin-left duration-200`}
+              />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 {Menu.title}
               </span>
